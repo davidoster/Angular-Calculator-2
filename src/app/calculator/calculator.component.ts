a88aaa4fa676204653e67665d96ee37b0bad1244
David Oster <paspa@hotmail.com>
2021-01-20 12:35:45 +0200

    buidling the main functionality

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -8,17 +8,68 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class CalculatorComponent implements OnInit {
   title: string = "Angular Calculator!!!";
-  count: number = 20;
-  finalValue: number = 0;
+  firstValue: any; // 1st operand
+  secondValue: any; // 2nd operand
+  operator: any; // operation
+  lcdValue: any; // lcd display
+  maxChars: number = 4;
 
-  constructor() { }
+  constructor() { 
+    console.log(this.title);
+    this.clear();
+    console.log(this.operator);
+
+  }
 
   ngOnInit() {
-    console.log(this.title);
+    
+  }
+
+  getNumber(value) {
+    if(this.operator == null) {
+      if(this.firstValue == 0) {
+        this.firstValue = value;
+        //console.log(this.firstValue);
+      } else {
+        this.firstValue =  this.firstValue + '' + value;
+        //console.log(this.firstValue);
+      }
+      this.lcdValue = this.firstValue;
+    } else {
+      if(this.secondValue == 0) {
+        this.secondValue = value;
+        console.log(this.secondValue);
+      } else {
+        this.secondValue =  this.secondValue + '' + value;
+        console.log(this.secondValue);
+      }
+      this.lcdValue = this.secondValue;
+    }
+  }
+
+  getOperator(value) {
+    this.operator = value;
+    console.log(this.operator);
+  }
+
+  calculate() {
+    switch(this.operator) {
+      case '+':
+        this.lcdValue = this.firstValue + this.secondValue;
+        break;
+    }
+    
   }
 
   type(value) {
-    this.finalValue = value;
+    this.lcdValue /= value;
+  }
+
+  clear() {
+    this.firstValue = 0;
+    this.secondValue = 0;
+    this.operator = null;
+    this.lcdValue = 0;
   }
   
 }
